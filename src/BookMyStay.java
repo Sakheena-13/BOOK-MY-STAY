8b82aaa9451ffb0364b25cf8c806f10d9caa7cbc
sakheena_13 <sakheenburugula@gmail.com>
2026-03-13 09:15:40 +0530

    Base application setup

---
+++ src/BookMyStay.java
@@ -0,0 +1,23 @@
+/**
+ * The BookMyStay class serves as the entry point for the
+ * Hotel Booking Management System.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class BookMyStay {
+
+    /**
+     * The main method is the starting point of the application.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Welcome message along with the application name and version
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("Application Name: Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+
+        // Application terminates after execution
+    }
+}
